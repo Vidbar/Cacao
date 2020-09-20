43016cc486b908290f31946c1922778a03a41d0c
David <barrantes.d@gmail.com>
2020-09-20 12:20:04 -0600

    Remove unused classes

--- src/packages/cacao/elements/basicElements/LeafBasicElement.kt
+++
@@ -1,9 +0,0 @@
-package packages.cacao.elements.basicElements
-
-import packages.cacao.widgets.BasicWidget
-
-open class LeafBasicElement(widget: BasicWidget) : BasicElement(widget) {
-    override fun performRebuild() {
-        TODO("Not yet implemented")
-    }
-}
--- src/packages/cacao/widgets/SingleChildRenderWidget.kt
+++
@@ -1,8 +0,0 @@
-package packages.cacao.widgets
-
-import packages.cacao.elements.renderElements.SingleChildRenderElement
-
-abstract class SingleChildRenderWidget : RenderWidget() {
-    abstract var child: Widget
-    override fun createElement(): SingleChildRenderElement = SingleChildRenderElement(this)
-}
